5d640be383e205b5b5fc1b290bfa8bc940bc9fc9
James R Glasgow <james.glasgow@associates.hq.dhs.gov>
2022-09-13 13:02:52 -0600

    fixing the message "PHP Deprecated:  Return type of PHPExiftool\Reader::getIterator() should either be compatible with IteratorAggregate::getIterator(): Traversable, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in /var/www/vendor/alchemy/phpexiftool/lib/PHPExiftool/Reader.php on line 119"

--- lib/PHPExiftool/Reader.php
+++ lib/PHPExiftool/Reader.php
@@ -116,7 +116,7 @@ class Reader implements \IteratorAggregate
      *
      * @return \Iterator
      */
-    public function getIterator()
+    public function getIterator(): \Iterator
     {
         return $this->all()->getIterator();
     }
